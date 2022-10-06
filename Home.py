f0e8164817e5912288c754decc95ccc2377ba9e3
NikolatSun <111493437+NikolatSun@users.noreply.github.com>
2022-10-06 22:25:29 +0500

    DZ

---
+++ Home.py
@@ -0,0 +1,19 @@
+# print("Hello")
+
+
+# while True:
+#     password = input("password 1:")
+#     password2 = input("password 2:")
+#     a = True
+#     if len(password)<8:
+#         a = False
+#         print("Короткий")
+#     if password != password2:
+#         a = False
+#         print("Различаются")
+#     if "123" in password:
+#         a = False
+#         print("Простой")
+#     if a:
+#         break
+# print("OK")
