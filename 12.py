e226d8b3b03bfd5c84bb503689d2fdb05c538010
NikolatSun <111493437+NikolatSun@users.noreply.github.com>
2022-11-09 18:24:35 +0500

    6

--- 12.py
+++ 12.py
@@ -26,26 +26,3 @@ for t in threads:
     t.join()
 
 print("Время на выстрел:", round(time.time() - t2, 2), "секунд")
-
-
-def get_html(url):
-    text = requests.get(url).text
-    print(f'{url} text length is {len(text)}')
-
-
-urls = ['https://pythonru.com/', 'https://www.google.com/',
-        'https://github.com/', 'https://brunoyam.com/', 'https://yandex.ru/']
-threads = [Thread(target=get_html, args=(urls[i], )) for i in range(5)]
-t1 = time.time()
-for k in threads:
-    k.start()
-    k.join()
-print(f'Время последовательного выполнения: {time.time()-t1}\n')
-
-threads = [Thread(target=get_html, args=(urls[i], )) for i in range(5)]
-t1 = time.time()
-for k in threads:
-    k.start()
-for k in threads:
-    k.join()
-print(f'Время параллельного выполнения: {time.time()-t1}')
